0a26b377e925248b71d5f21054855cdd9e04bb15
Student <azaryanmail@gmail.com>
2024-06-04 01:29:09 +0300

    add centr

--- src/components/Central/Central.jsx
+++ src/components/Central/Central.jsx
@@ -1,10 +1,53 @@
 export default function Central() {
   return (
     <div className="central">
-      <h3 className="central">Central</h3>
-      <h3 className="central">Central</h3>
-      <h3 className="central">Central</h3>
-      <h3 className="central">Central</h3>
+      <div className="central__wrap">
+        <div className="central__text">
+          <h2 className="central__title">
+            Без проблемная доставка купленных вещей из США, Европы и Азии
+            в Украины
+          </h2>
+          <p className="central__paragraph">
+            Сэкономьте на покупках и наслаждайтесь мировыми брендами —
+            наш сервис позволяет вам легко и удобно заказывать товары со всего
+            мира и получать их в Украины
+          </p>
+        </div>
+        <div className="central-img">
+          <img
+            className="central-img__rectangle"
+            src="./img/rectangle.png"
+            alt="photo"
+          />
+        </div>
+      </div>
+
+      <div className="central__form">
+        <div className="central__form__text">
+          <h3 className="central__form__title">Калькулятор доставки</h3>
+
+          <p className="central__form__paragraph">
+            Посчитайте, сколько будет стоить перевозка покупки из зарубежа
+          </p>
+        </div>
+
+        {/*  form start*/}
+        <div className="central__form__group">
+          <div className="form__name_wrap">
+            <input type="text" className="form__name form" placeholder="Имя" />
+          </div>
+          <input type="text" className="form" placeholder="Почта" />
+          <input type="text" className="form" placeholder="Телефон" />
+          <input type="text" className="form" placeholder="Общая площадь, м²" />
+          <input type="text" className="form" placeholder="Вес, кг" />
+          <input type="text" className="form" placeholder="Страна покупки" />
+          <input type="text" className="form" placeholder="Город покупки" />
+          <input type="text" className="form" placeholder="Область доставки" />
+          <input type="text" className="form" placeholder="Город доставки" />
+          <div className="btn btn_form">Заказать расчёт</div>
+        </div>
+        {/*  form end*/}
+      </div>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <header className="header">
-      <div className="header__wrap header__wrap_bottom">
+      <div className="header__wrap">
         <div className="header__container">
           <div className="header__logo">
             <img
               className="header__logo__img"
-              // src="/img/vector.png"
               src="./img/vector.png"
               alt="logo"
             />
@@ -14,38 +21,56 @@ export default function Header() {
           <h2 className="header-title__logo">fromBoard Delivery</h2>
         </div>
 
-        <div className="header__container">
-          <div className="header__menu">
-            <ul className="header__menu__ul header__menu__ul_hidden">
-              <li>Доставляем товары из заграницы в Украину</li>
-              <li>Стоимость от 550 гривен за заказ</li>
-              <li>1 $ = 93 ₽ | 1 € = 99 ₽ | 1 ¥ = 13 ₽</li>
-            </ul>
-            <div className="trait trait_hidden"></div>
-            <ul className="header__menu__ul">
-              <li className="header__mobile">+3 (800) 123 45-67</li>
-              <li className="header__city header__city_hidden">
-                Звонки по Украине бесплатны
-              </li>
-            </ul>
-          </div>
-          <div className="menu-toggle">
-            <div className="bar"></div>
-            <div className="bar"></div>
-            <div className="bar"></div>
-          </div>
-        </div>
+        <nav>
+          <a
+            href="#"
+            className="menu-toggle"
+            id="burger"
+            onClick={toggleVisibility}
+          >
+            {isVisible ? (
+              <div className="close">x</div>
+            ) : (
+              <div>
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+              </div>
+            )}
+          </a>
+        </nav>
       </div>
 
-      <div className="header__nav header__wrap">
-        <ul className="header__nav__ul">
-          <li className="nav-item nav-item_calc">Калькулятор</li>
-          <li className="nav-item">О нас</li>
-          <li className="nav-item">Гарантии</li>
-          <li className="nav-item">Отзывы</li>
-        </ul>
-        <button className="btn">Оставить заявку</button>
-      </div>
+      {isVisible && (
+        <div>
+          <div className="header__nav">
+            <ul className="header__nav__ul">
+              <li className="nav-item nav-item_calc">Калькулятор</li>
+              {/* <li className="nav-item">О нас</li> */}
+              {/* <li className="nav-item">Гарантии</li> */}
+              {/* <li className="nav-item">Отзывы</li> */}
+            </ul>
+            <button className="btn">Оставить заявку</button>
+          </div>
+
+          <div className="header__price">
+            {/* <ul className="header__list">
+            <li>Доставляем товары из заграницы в Украину</li>
+            <li>Стоимость от 550 гривен за заказ</li>
+            <li>1 $ = 93 ₽ | 1 € = 99 ₽ | 1 ¥ = 13 ₽</li>
+          </ul> */}
+
+            {/* <div className="trait trait_hidden"></div> */}
+
+            {/* <ul className="header__connection">
+            <li className="header__mobile">+3 (800) 123 45-67</li>
+            <li className="header__city">Звонки по Украине бесплатны</li>
+          </ul> */}
+          </div>
+          {/*  */}
+        </div>
+      )}
+      {/*  */}
     </header>
   );
 }
