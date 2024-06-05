377cfd129d08ae8252113fc036e369d178b0912b
Student <azaryanmail@gmail.com>
2024-06-05 15:30:30 +0300

    chande header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,6 +21,23 @@ export default function Header() {
           <h2 className="header-title__logo">fromBoard Delivery</h2>
         </div>
 
+        <div className="header__oll__blok" id="burger">
+          <div className="header__price">
+            <ul className="header__list">
+              <li>Доставляем товары из заграницы в Украину</li>
+              <li>Стоимость от 550 гривен за заказ</li>
+              <li>1 $ = 93 ₽ | 1 € = 99 ₽ | 1 ¥ = 13 ₽</li>
+            </ul>
+
+            <div className="trait trait_hidden"></div>
+
+            <ul className="header__connection">
+              <li className="header__mobile">+3 (800) 123 45-67</li>
+              <li className="header__city">Звонки по Украине бесплатны</li>
+            </ul>
+          </div>
+        </div>
+
         <nav>
           <a
             href="#"
@@ -41,35 +58,32 @@ export default function Header() {
         </nav>
       </div>
 
+      {/*  */}
+      <div className="header__blok">
+        <div className="header__blok__nav">
+          <ul className="header__nav__ul">
+            <li className="nav-item nav-item_calc">Калькулятор</li>
+            <li className="nav-item">О нас</li>
+            <li className="nav-item">Гарантии</li>
+            <li className="nav-item">Отзывы</li>
+          </ul>
+        </div>
+        <button className="btn">Оставить заявку</button>
+      </div>
+      {/*  */}
+
       {isVisible && (
         <div className="header__content">
           <div className="header__nav">
             <ul className="header__nav__ul">
               <li className="nav-item nav-item_calc">Калькулятор</li>
-              {/* <li className="nav-item">О нас</li> */}
-              {/* <li className="nav-item">Гарантии</li> */}
-              {/* <li className="nav-item">Отзывы</li> */}
             </ul>
             <button className="btn">Оставить заявку</button>
           </div>
 
-          <div className="header__price">
-            {/* <ul className="header__list">
-            <li>Доставляем товары из заграницы в Украину</li>
-            <li>Стоимость от 550 гривен за заказ</li>
-            <li>1 $ = 93 ₽ | 1 € = 99 ₽ | 1 ¥ = 13 ₽</li>
-          </ul> */}
-
-            {/* <div className="trait trait_hidden"></div> */}
-
-            {/* <ul className="header__connection">
-            <li className="header__mobile">+3 (800) 123 45-67</li>
-            <li className="header__city">Звонки по Украине бесплатны</li>
-          </ul> */}
-          </div>
+          <div className="header__price"></div>
         </div>
       )}
-      {/*  */}
     </header>
   );
 }
