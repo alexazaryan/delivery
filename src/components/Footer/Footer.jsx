3d1569ab9eb981630ae543c1c6f36ef1e7b02b5e
Student <azaryanmail@gmail.com>
2024-06-12 21:08:11 +0300

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,30 @@
 export default function Footer() {
   return (
-    <div className="footer">
-      <h3>footer</h3>
+    <div className="footer__container">
+      <div className="footer">
+        <div className="footer__wrap_left">
+          <div className="footer__box__img">
+            <img src="./img/vector.png" className="footer__img" lt="logo" />
+          </div>
+
+          <p>FromBoard Delivery</p>
+        </div>
+
+        <div className="footer__wrap_right">
+          <ul className="footer-box__ul">
+            <li className="footer__tel">+3 (800) 123 45-67</li>
+            <li>Звонки по Украине бесплатны</li>
+          </ul>
+        </div>
+
+        <p className="footer__text__p">
+          НЕ ЯВЛЯЕТСЯ ПУБЛИЧНОЙ ОФЕРТОЙ. Просим обратить внимание на то, что
+          цены носят информационный и ознакомительный характер, а значит ни в
+          какой мере не являются публичной офертой, которая определена. Вся
+          информация на сайте может содержать неточности, орфографические и иные
+          ошибки, она не является полной, окончательно и исчерпывающей
+        </p>
+      </div>
     </div>
   );
 }
